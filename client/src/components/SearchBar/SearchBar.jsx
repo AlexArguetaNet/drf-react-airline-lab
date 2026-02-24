96d6d072de2ff122498414cfa577ef024b32910f
AlexArguetaNet <alexarguetanet@gmail.com>
2026-02-23 20:59:35 -0500

    Implemented search feature
    The search bar on the home page filters out
    a single flight based on the flight id (flight number).
    A copy of the flights list is used to display the flights
    on the page. The original list stored in flights in Home.jsx
    is never modified.

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import "./SearchBar.css"
 import { IoIosSearch } from "react-icons/io";
 
-function SearchBar({ query, handleSearch }) {
+function SearchBar({ handleSearch, resetFlightList }) {
+
+    const [query, setQuery] = useState("");
+
+    const handleClick = () => {
+        handleSearch(query);
+    }
+
+    const handleChange = (e) => {
+
+        if (e.target.value != "") {
+            setQuery(e.target.value);
+        } else {
+            resetFlightList();
+            setQuery("");
+        }
+
+    }
 
     return (
         <div id="search-bar">
-            <input type="number" placeholder="Enter flight number" />
-            <button>
+            <input type="text" placeholder="Enter flight number" onChange={(e) => handleChange(e)} value={query} />
+            <button onClick={handleClick}>
                 <IoIosSearch />
             </button>
         </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,22 +7,42 @@ import { useState, useEffect } from "react"
 function Home() {
 
     const [flights, setFlights] = useState([]);
+    const [flightsFiltered, setFlightsFiltered] = useState([]);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const handleFetchFlights = async () => {
             const flights = await fetchFlights();
 
             setFlights(flights);
+            setFlightsFiltered(flights);
 
         }
         handleFetchFlights();
     }, []);
 
+    const search = (query) => {
+        const results = flights.filter(flight => flight.id == query);
+        setFlightsFiltered(results);
+    }
+
+    const resetFlightsList = () => {
+        setFlightsFiltered(flights);
+    }
+
+    const displayList = () => {
+        if (flightsFiltered.length > 0) {
+            return <FlightList flights={flightsFiltered} />
+        } else {
+            return <p>No results</p>
+        }
+    }
+
     return (
         <Page secondClassName={"home-page"}>
-            <SearchBar />
+            <SearchBar handleSearch={search} resetFlightList={resetFlightsList}/>
             {
-                flights ?  <FlightList flights={flights} />
+                flightsFiltered ?  displayList()
                         : <p>No flights</p>
             }
         </Page>
